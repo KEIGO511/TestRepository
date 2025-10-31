5cba1a89947ca0ea60833bb237964f84ec7c0676
keigo <keigo21511@gmail.com>
2025-10-31 10:14:39 +0900

    Fixes app.py about WTForms

--- forms.py
+++ forms.py
@@ -9,28 +9,39 @@ from wtforms.validators import (
 )
 
 class UserInfoForm(FlaskForm):
-    name = StringField('名前：',validators=[DataRequired('名前は入力必須です')]
-                       ,render_kw={"placeholder":"（例）フラスク太郎"})
+    name = StringField(
+        '名前：',
+        validators=[DataRequired(message='名前は入力必須です')],
+        render_kw={"placeholder":"（例）フラスク太郎"}
+        )
     
-    age = IntegerField('年齢：',validators=[NumberRange(min=18, max=100, message='入力範囲は18歳から100歳です')]
-                       ,default=20)
+    age = IntegerField(
+        '年齢：',
+        validators=[NumberRange(min=18, max=100, message='入力範囲は18歳から100歳です')],
+        default=20
+        )
     
-    password = PasswordField('パスワード；',
-                             validators=[Length(min=1, max=10, message='パスワードの長さは1文字以上10文字以内です'),
-                                         EqualTo('confirm_password',message='確認用パスワードと一致しません')
-                                         ]
-                             )
+    password = PasswordField(
+        'パスワード；',
+        validators=[
+            Length(min=1, max=10, message='パスワードの長さは1文字以上10文字以内です'),
+            EqualTo('confirm_password',message='確認用パスワードと一致しません')
+            ]
+        )
+    
     confirm_password = PasswordField('パスワード確認： ')
     
-    email = EmailField('メールアドレス：',
-                       validators=[Email('有効なメールアドレスを入力してください')
-                                   ]
-                       )
+    email = EmailField(
+        'メールアドレス：',
+        validators=[Email(message='有効なメールアドレスを入力してください')]
+        )
     
-    birthday = DateField('生年月日：',
-                         validators=[DataRequired('生年月日は入力必須です')],
-                         format="%Y-%m-%d",
-                         render_kw={"placeholder":"yyyy/mm/dd"})
+    birthday = DateField(
+        '生年月日：',
+        validators=[DataRequired(message='生年月日は入力必須です')],
+        format="%Y-%m-%d",
+        render_kw={"placeholder":"yyyy/mm/dd"}
+        )
     
     gender = RadioField('性別：',choices=[('man','男性'),('woman','女性')],default='man')
     
